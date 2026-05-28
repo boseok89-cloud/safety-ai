492df874fd3b586c397557498a548e2638d102e3
boseok89-cloud <boseok89@gmail.com>
2026-05-28 10:35:50 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Admin from './Admin';
+
 const isAdmin = window.location.pathname === '/admin';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode>{isAdmin ? <Admin /> : <App />}</React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    {isAdmin ? <Admin /> : <App />}
+  </React.StrictMode>
+);
